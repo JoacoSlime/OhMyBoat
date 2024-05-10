38d3afedf93b821e087af6702cd86b960e2719a5
holafrank <gonzalolucchi2@gmail.com>
2024-05-10 16:48:41 -0300

    SignInCliente no quiere colaborar

--- OhMyBoat.UI/Shared/Entidades/Cliente.cs
+++ OhMyBoat.UI/Shared/Entidades/Cliente.cs
@@ -8,6 +8,20 @@ namespace OhMyBoat.UI.Shared.Entidades
 {
     public class Cliente : Usuario
     {
+
+        public Cliente(){}
+
+        //es necesario o no el constructor? genera ambiguedades? necesito ese constructor vacio para que me deje de joder?
+        public Cliente (String nombre, String email, String passw){
+            Nombre = nombre;
+            Email = email;
+            Password = passw;
+            UserName = email.Remove(email.IndexOf('@'), email.Length-1);
+            MayorDeEdad = true;
+            Bloqueado = false;
+            Contacto = null;
+            Rol = Roles.Cliente;
+        }
         public List<Terrestre>? Terrestres {  get; set; }
         public List<Maritimo>? Maritimos {  get; set; }
     }
