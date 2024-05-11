5f09d22e1cb20839bde3630a8aa621cb1472e6a3
holafrank <gonzalolucchi2@gmail.com>
2024-05-11 10:22:52 -0300

    Repetir contraseña no era necesario

--- OhMyBoat.UI/Shared/Entidades/Cliente.cs
+++ OhMyBoat.UI/Shared/Entidades/Cliente.cs
@@ -9,13 +9,22 @@ namespace OhMyBoat.UI.Shared.Entidades
     public class Cliente : Usuario
     {
 
-        public Cliente(){}
+        public Cliente(){
+            String email = Email;
+            UserName = email.Remove(email.IndexOf('@'), email.Length-1);
+            MayorDeEdad = true;
+            Bloqueado = false;
+            Contacto = null;
+            Rol = Roles.Cliente;
+            String p = Password;
+        }
 
         //es necesario o no el constructor? genera ambiguedades? necesito ese constructor vacio para que me deje de joder?
         public Cliente (String nombre, String email, String passw){
             Nombre = nombre;
             Email = email;
             Password = passw;
+            String em = Email;
             UserName = email.Remove(email.IndexOf('@'), email.Length-1);
             MayorDeEdad = true;
             Bloqueado = false;
