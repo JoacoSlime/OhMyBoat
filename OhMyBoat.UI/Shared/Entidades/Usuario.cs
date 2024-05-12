c21e2196934656d09ba14d0264af4722c795b6b6
holafrank <gonzalolucchi2@gmail.com>
2024-05-12 19:45:27 -0300

    Email valido y reparacion de constructores

--- OhMyBoat.UI/Shared/Entidades/Usuario.cs
+++ OhMyBoat.UI/Shared/Entidades/Usuario.cs
@@ -13,7 +13,6 @@ namespace OhMyBoat.UI.Shared.Entidades
         public string Email { get; set; } = "";
         public string UserName { get; set; } = "";
         public string Password { get; set; } = "";
-        public bool MayorDeEdad { get; set; }
         public bool Bloqueado { get; set; } = false;
         public string? Contacto { get; set; }
         public Roles Rol { get; set; }
