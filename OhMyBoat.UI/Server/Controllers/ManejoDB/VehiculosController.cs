6fd9296fc500296c2d092ee0df7ad40a91d7bb3f
holafrank <gonzalolucchi2@gmail.com>
2024-05-16 16:52:26 -0300

    agrego vehiculos y barquitos controller los cuales no andan pero bueno se vera

--- OhMyBoat.UI/Server/Controllers/ManejoDB/VehiculosController.cs
+++ OhMyBoat.UI/Server/Controllers/ManejoDB/VehiculosController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OhMyBoat.UI.Server.Data;
 
 namespace OhMyBoat.UI.Server.Controllers.ManejoDB
 {
@@ -7,5 +9,26 @@ namespace OhMyBoat.UI.Server.Controllers.ManejoDB
     [ApiController]
     public class VehiculosController : ControllerBase
     {
+    }
+
+    [HttpGet]
+    [Route("ListarNaviosDisponibles")]
+    public async Task<IActionResult> GetMaritimos()
+    {
+        using (var db = new OhMyBoatUIServerContext())
+        {
+            var listar_nav = await db.Barquitos.OrderBy(bar => bar.Matricula).ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, listar_nav);
+        }
+    }
+
+    [HttpGet]
+    [Route("ListarNaviosDisponibles")]
+    public async Task<IActionResult> GetTerrestres(){
+        using (var bd = new OhMyBoatUIServerContext()){
+            var listar_ter = await bd.Terrestres.OrderBy(terr => terr.Matricula).ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, listar_ter); 
+        }
+
     }
 }
