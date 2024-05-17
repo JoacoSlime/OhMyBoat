53e639e3959d20650e997da82657be3fb631b330
morito <bernabe.moro197730@alumnos.info.unlp.edu.ar>
2024-05-17 18:59:45 -0300

    solucionado no se bindeaba el vehiculo con el clinte (id)

--- OhMyBoat.UI/Shared/Entidades/Vehiculo.cs
+++ OhMyBoat.UI/Shared/Entidades/Vehiculo.cs
@@ -9,7 +9,7 @@ namespace OhMyBoat.UI.Shared.Entidades
     public class Vehiculo
     {
         public int Id { get; set; }
-        public int IDCliente { get; set; }
+        public string  IDCliente { get; set; } ="";
         public string Matricula { get; set; } = "";
         public string? base64imagen {  get; set; }
         public int Antiguedad { get; set; }
