cd8514f742dc4283ce8296953cfeeefc741efcd9
JoacoSlime <JoacoSlime@proton.me>
2024-05-21 12:09:39 -0300

    Top10CosasAlPedoDelAnime

--- OhMyBoat.UI/Server/Controllers/ManejoDB/CuentaController.cs
+++ OhMyBoat.UI/Server/Controllers/ManejoDB/CuentaController.cs
@@ -148,13 +148,13 @@ namespace OhMyBoat.UI.Server.Controllers.ManejoDB
 
         [HttpPost]
         [Route("SwitchUserBlock")]
-        public async Task<IActionResult> BloaquearUsuario([FromBody] LoginDTO log)
+        public async Task<IActionResult> BloaquearUsuario([FromBody] Cliente client)
         {
             using var db = new OhMyBoatUIServerContext();
-            var user = await db.Usuarios.Where(u => u.Email == log.Email).FirstOrDefaultAsync();
-            if (user != null){
-                user.Bloqueado = user.Bloqueado;
-                db.Usuarios.Update(user);
+            var existe = await db.Usuarios.Where(u => u.Email == client.Email).AnyAsync();
+            if (existe){
+                client.Bloqueado = client.Bloqueado;
+                db.Usuarios.Update(client);
                 await db.SaveChangesAsync();
                 return StatusCode(StatusCodes.Status200OK);
             }
