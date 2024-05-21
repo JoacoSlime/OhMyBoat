187714cabbf604570e5d7bd1be3998475c829316
JoacoSlime <JoacoSlime@proton.me>
2024-05-20 21:23:18 -0300

    Gogogogogo

--- OhMyBoat.UI/Server/Program.cs
+++ OhMyBoat.UI/Server/Program.cs
@@ -6,6 +6,7 @@ using OhMyBoat.UI.Server.Data;
 using OhMyBoat.UI.Server.Services;
 using Microsoft.Extensions.Configuration;
 using OhMyBoat.UI.Server.Helpers;
+using OhMyBoat.UI.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -51,10 +52,10 @@ using (var context = new OhMyBoatUIServerContext()) // ESTO ES PARA QUE NO EXPLO
     {
         context.Usuarios.Add(new OhMyBoat.UI.Shared.Entidades.Usuario() { Email = "admin@admin.com",Password="925427659676ada68e35f680a1f4c8da1cee5955d9bd014772d4b798c5bae13a",Nombre="Daniel",Rol=OhMyBoat.UI.Shared.Entidades.Roles.jefe}); // contra Admin1234.
         context.Usuarios.Add(new OhMyBoat.UI.Shared.Entidades.Usuario() { Email = "empleado@empleado.com",Password="0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Nombre="Mariano",Rol=OhMyBoat.UI.Shared.Entidades.Roles.empleado});// contra Agua1234.
-        context.Clientes.Add(new OhMyBoat.UI.Shared.Entidades.Cliente() { Email = "cliente@cliente.com", Nombre="juan", Password = "0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Rol = OhMyBoat.UI.Shared.Entidades.Roles.cliente });// contra Agua1234.
-        context.Clientes.Add(new OhMyBoat.UI.Shared.Entidades.Cliente() { Email = "clientebloq1@cliente.com", Bloqueado = true, Nombre = "Pedro", Password = "0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Rol = OhMyBoat.UI.Shared.Entidades.Roles.cliente }) ;// contra Agua1234.
-        context.Clientes.Add(new OhMyBoat.UI.Shared.Entidades.Cliente() { Email = "clientebloq2@cliente.com", Bloqueado = true, Nombre ="silvia", Password = "0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Rol = OhMyBoat.UI.Shared.Entidades.Roles.cliente });// contra Agua1234.
-        context.Clientes.Add(new OhMyBoat.UI.Shared.Entidades.Cliente() { Email = "cliente2@cliente.com", Nombre="tom", Password = "0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Rol = OhMyBoat.UI.Shared.Entidades.Roles.cliente });// contra Agua1234.
+        context.Clientes.Add(new OhMyBoat.UI.Shared.Entidades.Cliente() { Email = "cliente@cliente.com", Nombre="juan", Password = "0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Rol = OhMyBoat.UI.Shared.Entidades.Roles.cliente});// contra Agua1234.
+        context.Clientes.Add(new OhMyBoat.UI.Shared.Entidades.Cliente() { Email = "clientebloq1@cliente.com", Bloqueado = true, Nombre = "Pedro", Password = "0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Rol = OhMyBoat.UI.Shared.Entidades.Roles.cliente});// contra Agua1234.
+        context.Clientes.Add(new OhMyBoat.UI.Shared.Entidades.Cliente() { Email = "clientebloq2@cliente.com", Bloqueado = true, Nombre ="silvia", Password = "0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Rol = OhMyBoat.UI.Shared.Entidades.Roles.cliente});// contra Agua1234.
+        context.Clientes.Add(new OhMyBoat.UI.Shared.Entidades.Cliente() { Email = "cliente2@cliente.com", Nombre="tom", Password = "0eb6f152d72087e15b6d61aa17ddafdce855f96330d56f004f129e4504d60c5d", Rol = OhMyBoat.UI.Shared.Entidades.Roles.cliente});// contra Agua1234.
         context.Tokens.Add(new OhMyBoat.UI.Shared.Entidades.TokenRecu() { Email = "admin@admin.com" ,StringAleatorioDelMomento = "papaya", Usado=false, FechaLimite= DateTime.Now.AddDays(3)}); // token valido que vence en 3 dias
         context.Tokens.Add(new OhMyBoat.UI.Shared.Entidades.TokenRecu() { Email = "empleado@empleado.com", StringAleatorioDelMomento = "papayavencida", Usado=false, FechaLimite= DateTime.Now}); // token valido vencido
         context.Tokens.Add(new OhMyBoat.UI.Shared.Entidades.TokenRecu() { Email = "cliente@cliente.com", StringAleatorioDelMomento = "papayausada", Usado=true, FechaLimite= DateTime.Now.AddDays(3)}); // token usado que vence en 3 dias
