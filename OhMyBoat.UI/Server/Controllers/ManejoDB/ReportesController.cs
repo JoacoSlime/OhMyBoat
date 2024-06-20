c5bd6bea8d3a2f797eccc8c1e38948cafecb5bfd
JoacoSlime <JoacoSlime@proton.me>
2024-06-20 20:46:08 -0300

    Reportar tipos de embarcaciones registradas implementado

--- OhMyBoat.UI/Server/Controllers/ManejoDB/ReportesController.cs
+++ OhMyBoat.UI/Server/Controllers/ManejoDB/ReportesController.cs
@@ -5,6 +5,7 @@ using OhMyBoat.UI.Server.Data;
 using OhMyBoat.UI.Server.Services;
 using OhMyBoat.UI.Shared;
 using OhMyBoat.UI.Shared.Entidades;
+using BlazorBootstrap;
 
 namespace OhMyBoat.UI.Server.Controllers.ManejoDB
 {
@@ -13,5 +14,15 @@ namespace OhMyBoat.UI.Server.Controllers.ManejoDB
     public class ReportesController : ControllerBase
     {
         
+        [HttpGet]
+        [Route("GetReporteTipoDeEmbarcacion")]
+        public async Task<IActionResult> GetReporteTipoDeEmbarcacion() {
+            using var db = new OhMyBoatUIServerContext();
+            List<double> dataList = new();
+            foreach(TipoEmbarcacion tipo in Enum.GetValues(typeof(TipoEmbarcacion))) { // Esto es una mierda -J
+                dataList.Add(await db.Maritimos.Where(maritimo => maritimo.Tipo == tipo).CountAsync());
+            }
+            return StatusCode(StatusCodes.Status200OK, dataList);
+        }
     }
 }
