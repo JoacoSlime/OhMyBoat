325f94290e83ff6c51806e6c7678cbb5721a8985
AniquiladorDeAchuras <eliasbontempi@gmail.com>
2024-06-20 20:34:50 -0300

    Implementacion del back de eliminar publicacion indebida

--- OhMyBoat.UI/Server/Controllers/ManejoDB/DenunciasController.cs
+++ OhMyBoat.UI/Server/Controllers/ManejoDB/DenunciasController.cs
@@ -26,13 +26,49 @@ namespace OhMyBoat.UI.Server.Controllers.ManejoDB
                                                                                         Cantidad = denuncias.Count(),
                                                                                         VehiculoId = aborto.VehiculoId,
                                                                                         EsNavio = aborto.EsNavio,
-                                                                                        
+
                                                                                     }).ToListAsync();
-            if(querysexosa != null)
+            if (querysexosa != null)
                 return StatusCode(StatusCodes.Status200OK, querysexosa);
             else
                 return StatusCode(StatusCodes.Status200OK, new List<DenunciasDTO>());
 
         }
+        [HttpPost]
+        [Route("EliminarPublicacion")]
+        public async Task<IActionResult> DenunciadoPapi([FromBody] DenunciasDTO dto)
+        {
+            using var db = new OhMyBoatUIServerContext();
+
+            Vehiculo? vehiculo = dto.EsNavio ? await db.Maritimos.Where(m => m.Id == dto.VehiculoId).FirstOrDefaultAsync() : await db.Terrestres.Where(t => t.Id == dto.VehiculoId).FirstOrDefaultAsync(); // ver si se borra bien
+            if (vehiculo != null)
+            {
+                var ofertas = await db.Ofertas.Where(o => (o.ID_VehiculoRecibeOferta == dto.VehiculoId && o.EsNavioRecibe == dto.EsNavio) ||
+                                                          (o.ID_VehiculoEnviaOferta == dto.VehiculoId && o.EsNavioEnvia == dto.EsNavio)).ToListAsync();
+                if (ofertas != null)
+                {
+                    foreach (var oferta in ofertas)
+                    {
+                        var turnitos = await db.Turno.Where(t => t.OfertaId == oferta.Id).ToListAsync();
+                        if (turnitos != null)
+                        {
+                            foreach (var turno in turnitos)
+                            {
+                                db.Remove(turno);
+                                await db.SaveChangesAsync();
+                            }
+                        }
+                        db.Remove(oferta);
+                        await db.SaveChangesAsync();
+                    }
+                }
+                db.Remove(vehiculo);
+                await db.SaveChangesAsync();
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            return StatusCode(StatusCodes.Status404NotFound);
+        }
+
+
     }
 }
