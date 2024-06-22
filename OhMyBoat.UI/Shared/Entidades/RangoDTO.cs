8ae6de441efe09a28d7514821e2255abb2e8c58d
AniquiladorDeAchuras <eliasbontempi@gmail.com>
2024-06-22 18:30:24 -0300

    ANULO MUFA pero si fuera bueno

--- OhMyBoat.UI/Shared/Entidades/RangoDTO.cs
+++ OhMyBoat.UI/Shared/Entidades/RangoDTO.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+<<<<<<< HEAD
 using System.ComponentModel;
+=======
+>>>>>>> cbf4364 (ANULO MUFA pero si fuera bueno)
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +12,12 @@ namespace OhMyBoat.UI.Shared.Entidades
 {
     public class RangoDTO
     {
+<<<<<<< HEAD
         public DateOnly inicio { get; set; }
         public DateOnly fin { get; set; }
+=======
+        public DateOnly inicio;
+        public DateOnly fin;
+>>>>>>> cbf4364 (ANULO MUFA pero si fuera bueno)
     }
 }
